4601c7b0e956b9d090a75a27dedd522f6fc2d6a5
sharonjvj <sharonjadelyn05@gmail.com>
2025-11-15 18:33:37 +0700

    Add cors

--- movie-express/app.js
+++ movie-express/app.js
@@ -1,16 +1,14 @@
 import express from 'express';
 import api from "./routes/api.js";
+import cors from 'cors';
 import database from "./config/database.js";
 
 const app = express();
+
+app.use(cors());
 app.use(express.json());
 app.use("/api", api);
 
-// app.get('/', (req, res) => {
-//     res.status(200).json({
-//         message : "OK"
-//     })
-// })
 
 app.listen(3000, () => {
     database();
